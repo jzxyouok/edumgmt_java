f1d8066ecd89729da1de6f6431f242abdc9cdf5d
guoshijie <guoshijie@shinc.net>
2015-08-03 18:38:15 +0800

    commit

--- edumgmt/src/test/java/net/shinc/common/HttpRequestTest.java
+++ edumgmt/src/test/java/net/shinc/common/HttpRequestTest.java
@@ -58,9 +58,7 @@ public class HttpRequestTest {
 		} catch (Exception e) {
 			System.out.println("发送 POST 请求出现异常！" + e);
 			e.printStackTrace();
-		}
-		// 使用finally块来关闭输出流、输入流
-		finally {
+		} finally {
 			try {
 				if (out != null) {
 					out.close();
