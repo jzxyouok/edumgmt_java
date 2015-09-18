066f318638a875e9e80d9a331f384d659b45a2f4
wangzhiying <wangzhiying@localhost.com>
2015-09-18 14:42:11 +0800

    uy

--- edumgmt/src/main/java/net/shinc/controller/edu/recommend/RecommendController.java
+++ edumgmt/src/main/java/net/shinc/controller/edu/recommend/RecommendController.java
@@ -148,6 +148,7 @@ public class RecommendController extends AbstractBaseController {
 	@ResponseBody
 	public IRestMessage updateRecommend(@Valid Recommend recommend, BindingResult bindingResult) {
 		IRestMessage iRestMessage = getRestMessage();
+		recommend.setId(2);
 		if (bindingResult.hasErrors()) {
 			iRestMessage.setDetail(ShincUtil.getErrorFields(bindingResult));
 			return iRestMessage;
