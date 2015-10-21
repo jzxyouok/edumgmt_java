40569b302e92a9d097660d36817ed00636ed3f0f
zhangtaichao <taichao1988@foxmail.com>
2015-10-21 14:57:31 +0800

    session

--- edumgmt/src/main/java/net/shinc/config/ContainerConfig.java
+++ edumgmt/src/main/java/net/shinc/config/ContainerConfig.java
@@ -11,7 +11,6 @@ import org.springframework.boot.context.embedded.ServletContextInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
 public class ContainerConfig implements ServletContextInitializer {
 
 	@Value("${session.timeout.minutes}")
